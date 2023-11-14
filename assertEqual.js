b8c5d4e88ab9ab09fcf4b7ff6d038b6da4db1914
Kimberley <kimberleychoicendlovu@gmail.com>
2023-11-14 23:34:50 +0000

    Completd the assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log("🎉🎉🎉Assertion passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑Assertion failed: " + actual + " !== " + expected);
+  }
+};
+
+//test code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
